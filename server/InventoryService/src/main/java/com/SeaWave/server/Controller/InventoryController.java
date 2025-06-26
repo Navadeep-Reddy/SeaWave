03c7ec0028ecd699309d6414b179bee4ecd8b428
Navadeep-Reddy <vnavadeepreddysatti@gmail.com>
2025-06-27 01:44:46 +0530

    Refactored server into InventoryService Microservice and added BookingService

--- server/InventoryService/src/main/java/com/SeaWave/server/Controller/InventoryController.java
+++ server/InventoryService/src/main/java/com/SeaWave/server/Controller/InventoryController.java
@@ -5,16 +5,14 @@ import com.SeaWave.server.Response.EventInventoryResponse;
 import com.SeaWave.server.Response.VenueLocationResponse;
 import com.SeaWave.server.Service.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin
 public class InventoryController {
 
     private final InventoryService inventoryService;
