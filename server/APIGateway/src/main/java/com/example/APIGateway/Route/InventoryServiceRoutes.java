c6575dc4d8d462ff6ed821f75f4cf2b90cdae746
Navadeep-Reddy <vnavadeepreddysatti@gmail.com>
2025-07-07 02:01:12 +0530

    Added gateway routing to get event by id

--- server/APIGateway/src/main/java/com/example/APIGateway/Route/InventoryServiceRoutes.java
+++ server/APIGateway/src/main/java/com/example/APIGateway/Route/InventoryServiceRoutes.java
@@ -14,6 +14,15 @@ public class InventoryServiceRoutes {
                 .route("get-all-events", r -> r
                         .path("/api/v2/inventory/events")
                         .filters(f -> f.rewritePath("/api/v2/inventory/events", "/api/v1/inventory/events"))
-                                .uri("http://localhost:8080")).build();
+                        .uri("http://localhost:8080"))
+                .route("get-venue-id", r -> r
+                        .path("/api/v2/venue/{venueId}")
+                        .filters(f -> f.rewritePath("/api/v2/venue/(?<venueId>.*)", "/api/v1/inventory/venue/${venueId}"))
+                        .uri("http://localhost:8080"))
+                .route("get-event-id", r -> r
+                        .path("/api/v2/inventory/event/{eventId}")
+                        .filters(f -> f.rewritePath("/api/v2/inventory/event/(?<eventId>.*)", "/api/v1/inventory/event/${eventId}"))
+                        .uri("http://localhost:8080"))
+                .build();
     }
 }
